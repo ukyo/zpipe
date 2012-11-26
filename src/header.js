2083c2702620d48acdab7aca57b9a764f6a69823
ukyo <ukyo.web@gmail.com>
2012-11-26 13:02:43 +0900

    Use TypedArray

--- src/header.js
+++ src/header.js
@@ -1,5 +1,24 @@
-!function (name, context, definition) {
-  if (typeof module !== 'undefined') module.exports = definition(name, context);
-  else if (typeof define === 'function' && typeof define.amd  === 'object') define(definition);
-  else context[name] = definition(name, context);
-}("zpipe", this, function(name, context) {
+var zpipe = {
+  run: function(input, isInflate) {
+    var i = -1;
+    var j = -1;
+    var output = new Uint8Array(1);
+    var temp;
+    var Module = {
+      arguments: isInflate ? ['-d'] : [],
+      stdin: function() {
+        return ++i < input.length ? input[i] : null;
+      },
+      stdout: function(x) {
+        if (x !== null) {
+          if (++j < output.length) {
+            temp = output;
+            output = new Uint8Array(output.length * 2);
+            output.set(temp);
+          } else {
+            output[j] = x;
+          }
+        }
+      }
+    };
+
