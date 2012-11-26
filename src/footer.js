5029c1432b20f395d406d0eec8156fd6d428d6d7
ukyo <ukyo.web@gmail.com>
2012-11-26 13:02:32 +0900

    Use TypedArray

--- src/footer.js
+++ src/footer.js
@@ -1,88 +1,11 @@
-function run(args) {
-  if (Module["preRun"]) {
-    Module["preRun"]();
-  }
+    return output.subarray(0, ++j);
+  },
 
-  args = args || Module["arguments"];
-  initRuntime();
-  var ret = null;
-  if (Module["_main"]) {
-    ret = Module.callMain(args);
-    exitRuntime();
-  }
+  deflate: function(input) {
+    return this.run(input);
+  },
 
-  if (Module["postRun"]) {
-    Module["postRun"]();
+  inflate: function (input) {
+    return this.run(input, true);
   }
-
-  return ret;
 };
-
-Module['run'] = run;
-
-// Make z'pipe!
-var old = context[name];
-
-var zpipe = (function() {
-	var data;
-	var i;
-	var output;
-
-	// Set up iostreams
-	FS.init(function() {
-		// Stdin								
-		if(i < data.length) {
-			var val = data.charCodeAt(i) & 0xFF;
-
-			i++;
-
-			return val;
-		} else {
-			return null;
-		}					
-	}, function(data) {
-		// Stdout			
-		output += String.fromCharCode(data & 0xFF);
-	});
-
-	// Export interface
-	return {
-		'inflate' : function(string) {
-			//
-			data = string;
-			i = 0;
-			output = '';
-
-			//			
-			if(run(['-d']) != 0) {
-				throw new Error("Could not inflate string");
-			}
-
-			return output;
-		},
-		'deflate' : function(string) {
-			data = string;
-			i = 0;
-			output = '';
-		
-			if(run([]) != 0) {
-				throw new Error("Could not deflate string");
-			}
-
-			return output;
-		},
-		'noConflict': function () {
-			if(old === undefined) {
-				delete context[name];				
-			} else {
-				context[name] = old;
-			}
-
-			return this;
-		}
-	};
-})();
-
-return zpipe;
-
-});
