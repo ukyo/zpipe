ed3c24c0bdae4ef55a61e37a0f6eea321f0c8d14
ukyo <ukyo.web@gmail.com>
2012-11-27 16:36:00 +0900

    update

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,26 @@
-    return output.subarray(0, ++j);
-  },
-
-  deflate: function(input) {
-    return this.run(input);
-  },
-
-  inflate: function (input) {
-    return this.run(input, true);
-  }
+  return new Uint8Array(zpipe_output.subarray(0, ++zpipe_j));
 };
+
+function deflate(input, level) {
+  return run.call({}, input, false, typeof level === 'number' ? level : 6);
+}
+
+function inflate(input) {
+  return run.call({}, input, true);
+}
+
+function gc() {
+  zpipe_buffer = new Uint8Array(1);
+}
+
+
+// export
+
+var global = (function() {return this})();
+
+global['zpipe'] = global['zpipe'] || {};
+global['zpipe']['deflate'] = deflate;
+global['zpipe']['inflate'] = inflate;
+global['zpipe']['gc'] = gc;
+
+})();
--- src/header.js
+++ src/header.js
@@ -1,24 +1,24 @@
-var zpipe = {
-  run: function(input, isInflate) {
-    var i = -1;
-    var j = -1;
-    var output = new Uint8Array(1);
-    var temp;
-    var Module = {
-      arguments: isInflate ? ['-d'] : [],
-      stdin: function() {
-        return ++i < input.length ? input[i] : null;
-      },
-      stdout: function(x) {
-        if (x !== null) {
-          if (++j < output.length) {
-            temp = output;
-            output = new Uint8Array(output.length * 2);
-            output.set(temp);
-          } else {
-            output[j] = x;
-          }
-        }
+(function() {
+
+var zpipe_buffer = new Uint8Array(1);
+
+function run(zpipe_input, zpipe_isInflate, zpipe_level) {
+  var zpipe_i = -1;
+  var zpipe_j = -1;
+  var zpipe_output = zpipe_buffer;
+  var Module = {};
+  Module['arguments'] = zpipe_isInflate ? ['-d'] : ['-c', '' + zpipe_level];
+  Module['stdin'] = function() {
+    return ++zpipe_i < zpipe_input.length ? zpipe_input[zpipe_i] : null;
+  };
+  Module['stdout'] = function(x) {
+    if (x !== null) {
+      if (++zpipe_j === zpipe_output.length) {
+        zpipe_buffer = new Uint8Array(zpipe_output.length * 2);
+        zpipe_buffer.set(zpipe_output);
+        zpipe_output = zpipe_buffer;
       }
-    };
+      zpipe_output[zpipe_j] = x;
+    }
+  };
 
