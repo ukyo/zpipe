53293032f62fce6e72c232808b7e9a54e2f10334
ukyo <ukyo.web@gmail.com>
2012-11-27 19:29:43 +0900

    use qunit

--- test/test.js
+++ test/test.js
@@ -1,54 +1,19 @@
-var 
-	chai = require('chai'),
-	assert = chai.assert,
-	zpipe = require("../"),
-	zlib = require("zlib");
+test('test zpipe.js', function() {
+	ok(zpipe, 'zpipe is exported');
+	ok(zpipe.deflate, 'zpipe.deflate is exported');
+	ok(zpipe.inflate, 'zpipe.inflate is exported');
+	ok(zpipe.gc, 'zpipe.gc is exported');
 
-var data = "Experiments With Alternating Currents of Very High Frequency, and Their Application to Methods of Artificial Illumination";
+	var bytes = new Uint8Array(10000);
+	for (var i = 0; i < 10000; ++i) bytes[i] = Math.floor(Math.random() * 255);
 
-describe("zpipe", function() {
-	describe("deflate", function() {
-		it("should compress strings", function() {
-			var zpipe_deflated = zpipe.deflate(data);
-
-			assert.ok(
-				zpipe_deflated !== undefined && zpipe_deflated.length > 0 && zpipe_deflated.length < data.length, 
-				"Did not compress data"
-			);		
-		});
-
-		it("should generate valid zlib input", function(done) {
-			var zpipe_deflated = zpipe.deflate(data);
-
-			zlib.inflate(new Buffer(zpipe_deflated, 'binary'), function(err, buffer) {
-				var zlib_inflated = buffer.toString('binary');
-
-				assert.ok(zlib_inflated == data, "Inflated data did not match input data");
-
-				done();
-			});
-		});
-	});
-
-	describe("inflate", function() {
-		it("should decompress strings", function() {
-			var zpipe_deflated = zpipe.deflate(data);
-
-			var zpipe_inflated = zpipe.inflate(zpipe_deflated);
-					
-			assert.ok(zpipe_inflated == data, "Inflated data did not match input data");			
-		});
-
-		it("should handle zlib output", function(done) {
-			zlib.deflate(new Buffer(data, 'binary'), function(err, buffer) {
-				var zlib_deflated = buffer.toString('binary');
-
-				var zpipe_inflated_zlib_deflated = zpipe.inflate(zlib_deflated);
-
-				assert.ok(zpipe_inflated_zlib_deflated == data, "zpipe did not inflate zlib output correctly");
-
-				done();
-			});
-		});	
-	});	
+	for (var level = 0; level <= 9; ++level) {
+		var def = zpipe.deflate(bytes, level);
+		var inf = zpipe.inflate(def);
+		equal(inf.length, bytes.length, 'byte lengths are same: compress level = ' + level);
+		for (var i = 0; i < 10000; ++i) if (inf[i] !== bytes[i]) break;
+		equal(i, 10000, 'all elements are same: compress level = ' + level);
+	}
 });
+
+start();
